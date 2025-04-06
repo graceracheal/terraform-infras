42f4bab3b42b80bdcaf470af6dc03732a9764f97
graceracheal <156642309+graceracheal@users.noreply.github.com>
2025-04-06 09:55:35 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
